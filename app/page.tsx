1593bcffb2fc2318201636d2345aae8a15ead484
Lennart Raach <Lennart.Raach@Student.Reutlingen-University.DE>
2023-10-25 22:54:26 +0200

    trying out css and its modules, also removed unnecessary files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import React from "react";
-export default function () {
+import styles from "styles/MainPage.module.css"
+export default function MainPage() {
     function Button(){
         let text = "Hi";
         if(20 + 5 === 25){
@@ -12,6 +13,7 @@ export default function () {
     }
     return(
         <main>
+            <div><h1 className={styles.WebsiteTitle}>RECHARGE THE SOURCE</h1></div>
             <h1> Hallo </h1>
             <Link href="/formular">Hier geht's zum Formular</Link>
             <Button></Button>
