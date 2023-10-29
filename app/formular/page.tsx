c8bf83311a6423ad4a99ad16fb9a6e53faef1a62
Anna Baur <anna.baur@student.reutlingen-university.de>
2023-10-29 18:39:25 +0100

    worked on the rough design

--- app/formular/page.tsx
+++ app/formular/page.tsx
@@ -1,9 +1,36 @@
 import Link from "next/link"
+import React from "react";
 export default function Formular () {
+    function Rectangle(){
+        const rectangleStyle = {
+            width: '900px',
+            height: '400px',
+            backgroundColor: 'darkgrey',
+        };
+        return(
+            <div style={rectangleStyle}>
+                <h1>Formular</h1>
+
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="name" />
+                <br/>
+                <label htmlFor="email">Email:</label>
+                <input type="text" id="email" name="email" />
+                <br/>
+                <label htmlFor="anmerkungen">Anmerkungen:</label>
+                <input type="text" id="anmerkungen" name="anmerkungen" />
+            </div>
+        )
+    }
+
     return (
         <main>
             <h1>Hier wird das Formular sein</h1>
             <Link href="/">Hier geht's wieder zurück zur Startseite</Link>
+
+            <Rectangle></Rectangle>
+
+
         </main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,46 @@ export default function MainPage() {
         }
         return(
             <button>{text}</button>
+        ) }
+    function Rectangle(){
+        const rectangleStyle = {
+        width: '400px',
+        height: '300px',
+        backgroundColor: 'darkgrey',
+    };
+        return(
+            <div style={rectangleStyle}>
+            Hier wird Text stehen
+            </div>
         )
     }
+
+
     return(
         <main>
-            <div><h1 className={styles.WebsiteTitle}>RECHARGE THE SOURCE</h1></div>
+            <header className={styles.Navigationbar}>Recycling | Facts | Überraschung | Kontakt</header>
+
+            <div><h1 className={styles.WebsiteTitle}>RECHARGE THE</h1></div>
+            <div><h1 className={styles.WebsiteTitle}>SOURCE</h1></div>
+
             <h1> Hallo </h1>
             <Link href="/formular">Hier geht's zum Formular</Link>
             <Button></Button>
+
+
+
+            <Rectangle></Rectangle>
+
+            <div>
+                <p className={styles.Text}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,<br />
+                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam<br />
+                    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, <br />
+                    no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur <br />
+                    sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, <br />
+                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd <br />
+                    gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            </div>
+
         </main>
     )
 }
