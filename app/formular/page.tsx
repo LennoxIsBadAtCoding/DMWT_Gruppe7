11983ccf7a68ceb952a6576856718449ecf6a806
Lennart Raach <Lennart.Raach@Student.Reutlingen-University.DE>
2023-12-24 11:49:54 +0100

    insert into database now possible, form now doesn't accept invalid inputs

--- app/formular/page.tsx
+++ app/formular/page.tsx
@@ -6,29 +6,11 @@ import styles from "styles/Formular.module.css";
 import Image from "next/image";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 export default function Formular () {
-    const fetcher = (url) => fetch(url).then((res) => res.json()); // gets an url, then fetches data from url and sends the data to res.json();
 
-    function FetchComments(){
-        const { data : comments,   // the received data
-                isLoading, // describes the current state of what to be fetched
-                error } =          // for when an error occurs
-                useSWR("https://jsonplaceholder.typicode.com/comments?_limit=10", fetcher); // the url with the limit of 10 and fetcher function
-        if (error) return <div>failed to load</div>
-        if (isLoading) return <div>loading...</div>
-        return (
-            <div id={styles.commentBox} className={styles.comment}> Kommentare:
-                <ol>
-                    {comments.map((comment, index) => (
-                        <li id={styles.comment} key={index}>
-                            {comment.name}
-                        </li>
-                    ))}
-                </ol>
-            </div>
-        );
-    }
     function Rectangle(){
         const rectangleStyle = {
             width: '900px',
@@ -51,17 +33,36 @@ export default function Formular () {
         )
     }
 
+    const SignupSchema = yup.object().shape({
+        firstName: yup.string().required("Please tell us, how we should address you"),
+        eMail: yup.string().required("Must be a valid E-Mail address")
+            .email("Must be a valid E-Mail address"),
+        userComment: yup.string().required("Please tell us what you want to know in the comment input")
+            .max(300, "The comment can't be longer than 300 characters")
+    });
     // form for user input
     function App() {
-        const {register, handleSubmit} = useForm();
+        const {register,
+            handleSubmit,
+            formState: {errors}
+        } = useForm({resolver: yupResolver(SignupSchema)});
         const [data, setData] = useState("");
 
         return (
             <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+                <div>
                 <input {...register("firstName")} placeholder="First name"/>
+                {errors.firstName && <p className={styles.errorMessage}>{errors.firstName.message}</p>}
+                </div>
+                <div>
                 <input {...register("eMail")} placeholder="E-mail"/>
-                <input {...register("comment")} placeholder="Comment"/>
-                <p>{data}</p>
+                {errors.eMail && <p className={styles.errorMessage}>{errors.eMail.message}</p>}
+                </div>
+                <div>
+                <input {...register("userComment")} placeholder="Comment"/>
+                {errors.userComment && <p className={styles.errorMessage}>{errors.userComment.message}</p>}
+                </div>
+                <p id={styles.inputData}>{data}</p>
                 <input type="submit"/>
             </form>
         );
@@ -69,14 +70,8 @@ export default function Formular () {
 
     return (
         <main>
-            <Rectangle></Rectangle>
             <App></App>
             <Link className={styles.linkStyle} href="/">Hier geht's wieder zurück zur Startseite</Link>
-            <FetchComments></FetchComments>
-            <Image       src="/meineErsteHeroSlideAnimation.gif"
-                         width={1920}
-                         height={1080}
-                         alt="Hero gif"/>
         </main>
     )
 }
